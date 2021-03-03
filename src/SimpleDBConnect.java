a6ffba0bb1df8d3a715445af161180f6d5680361
lejla-tudublin <lejla.rovcanin@tudublin.ie>
2021-03-03 12:01:02 +0000

    First Commit

--- src/SimpleDBConnect.java
+++ src/SimpleDBConnect.java
@@ -4,19 +4,21 @@
  * and open the template in the editor.
  */
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.ResultSetMetaData;
+
 /**
  *
  * @author lejla
  */
 public class SimpleDBConnect {
-    public SimpleDBConnect(){
-       // variables
+
+    public SimpleDBConnect() {
+        // variables
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
@@ -25,10 +27,9 @@ public class SimpleDBConnect {
 
         // Step 1: Loading or registering JDBC driver class
         try {
-           // Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-           Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-        }
-        catch(ClassNotFoundException cnfex) {
+            // Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+        } catch (ClassNotFoundException cnfex) {
             System.out.println("Problem in loading or "
                     + "registering MS Access JDBC driver");
             cnfex.printStackTrace();
@@ -42,43 +43,53 @@ public class SimpleDBConnect {
             statement = connection.createStatement();
 
             // Step 2.C: Executing SQL &amp; retrieve data into ResultSet
-            resultSet = statement.executeQuery("SELECT * FROM Department");
+            resultSet = statement.executeQuery("SELECT * FROM Project");
 
             // hardcoded header
-            System.out.println("#\tName\t\t\tManager\tStart");
-            System.out.println("=====\t=========\t=======\t=======");
-
+            //System.out.println("#\tName\t\t\tManager\tStart");
+            //System.out.println("=====\t=========\t=======\t=======");
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int numberOfColumns = metaData.getColumnCount();
+            // display the names of the columns in the ResultSet
+            for (int i = 1; i <= numberOfColumns; i++) {
+                System.out.printf("%-8s\t", metaData.getColumnName(i));
+            }
             // processing returned data and printing into console
             // Step 2.D: use data from ResultSet
-            while(resultSet.next()) {
-                System.out.println(resultSet.getString(1) + "\t" +
-                        resultSet.getString(2) + "\t\t" +
-                        resultSet.getString(3) + "\t" +
-                        resultSet.getString(4));
+            /* while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + "\t"
+                        + resultSet.getString(2) + "\t\t"
+                        + resultSet.getString(3) + "\t"
+                        + resultSet.getString(4));
+            }*/
+
+            while (resultSet.next()) {
+                System.out.println("");
+                for (int i = 1; i <= numberOfColumns; i++) {
+                    System.out.printf("%-8s\t", resultSet.getString(i));
+                }
             }
 
-        }
-        catch(SQLException sqlex){
+        } catch (SQLException sqlex) {
             System.err.println(sqlex.getMessage());
-        }
-        finally {
+        } finally {
 
-        // Step 3: Closing database connection
-        try {
-            if(null != connection) {
-                // cleanup resources, once after processing
-                resultSet.close();
-                statement.close();
-                // and then finally close connection
-                connection.close();
+            // Step 3: Closing database connection
+            try {
+                if (null != connection) {
+                    // cleanup resources, once after processing
+                    resultSet.close();
+                    statement.close();
+                    // and then finally close connection
+                    connection.close();
+                }
+            } catch (SQLException sqlex) {
+                System.err.println(sqlex.getMessage());
             }
-        }
-        catch (SQLException sqlex) {
-            System.err.println(sqlex.getMessage());
-        }
         }
     } // End of Constructor
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         new SimpleDBConnect();
     }
 } // End of Class
